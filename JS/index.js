e01cebb146a644bdd2a20b85d27d75ac1f9ac9e7
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-29 14:42:23 +0000

    init

---
+++ JS/index.js
@@ -0,0 +1,45 @@
+const input = document.querySelector("#usernameInput")
+const subBtn = document.querySelector(".signUpButton")
+const label = document.querySelector(".inputLabel")
+
+const url = "http://localhost:3000/newUser"
+
+const fetchToExpressServer = async(username)=>{
+    const headers = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            username: username
+        })
+    }
+    const response = await fetch(url, headers)
+    const result = response.json()
+    return result
+}
+
+const isSignedUp = JSON.parse(localStorage.getItem("SignedUpForMarioKart"))
+if(isSignedUp){
+    input.disabled = true
+    subBtn.disabled = true
+    input.placeholder = "Thanks for signing up!"
+    label.innerText = "You're allready signed up!"
+}
+
+subBtn.addEventListener("click", async ()=>{
+    const dataInput = input.value
+    input.value = "";
+    input.disabled = true;
+    label.innerText = "Saving Username, please wait!"
+    const data = await fetchToExpressServer(dataInput)
+    if (data.message){
+        label.innerText = data.message
+        input.placeholder = "Thanks for Signing up!"
+        subBtn.disabled = true
+        localStorage.setItem("SignedUpForMarioKart", JSON.stringify(true))
+    } else{
+        input.disabled = false;
+        label.innerText = "Please try again."
+    }
+
+    console.log(data)
+})
